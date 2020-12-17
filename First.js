da7ff4b268469520a178d7c39b3b2d8c32ec91a1
‪alaadeeb deeb95‬‏ <75927439+alaadib9@users.noreply.github.com>
2020-12-17 13:05:11 +0200

    Add files via upload

--- First.js
+++ First.js
@@ -1,3 +1,10 @@
 
-alert("Hi baby");
+alert("Hello and welcome");
 
+var YourAge= prompt(" Enter your age");
+if  ( YourAge >= 18 ) {
+   alert( "you are adult");
+
+   } else {
+       alert("You are underAge");
+}
